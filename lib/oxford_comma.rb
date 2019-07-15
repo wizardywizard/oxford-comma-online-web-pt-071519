21901f782fd9cab87a436066df180e2bec40806c
David Little <littledavid175@gmail.com>
2019-07-15 22:34:09 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+  elsif array.length == 2
+  array.join(" and ")
+  else
+    last = array.pop
+    array.join(", ") + ", and " + last
+  end
 end
